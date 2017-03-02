c3aea709ee4bd7c83a3efde46cf505bce5dd2295
GaboGomez09 <gomezgabriel09@gmail.com>
2017-03-02 01:17:50 -0600

    Initial Commit

---
+++ Generador-RFC-CURP/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Generador-RFC-CURP
+//
+//  Created by Gabriel gomez on 3/2/17.
+//  Copyright © 2017 Gabriel gomez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
