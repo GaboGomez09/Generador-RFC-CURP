bda560ddba15bbcc56376efcd010d53acc8bbc04
GaboGomez09 <gomezgabriel09@gmail.com>
2017-03-08 23:52:47 -0600

    Creación de funcionarios "generar_codigo_clave_por_nombre"

--- Generador-RFC-CURP/funciones.h
+++ Generador-RFC-CURP/funciones.h
@@ -9,5 +9,77 @@
 #ifndef funciones_h
 #define funciones_h
 
+void eliminar_caracteres_especiales(char **cadena){
+    char *cadena_sin_caracteres_especiales = (char*)malloc(50*sizeof(char));
+    int j = 0;
+    for (int i = 0; i < strlen(*cadena); i++) {
+        if (ispunct((*cadena)[i])) {
+        }else{
+            cadena_sin_caracteres_especiales[j] = (*cadena)[i];
+            j++;
+        }
+    }
+    strcpy((*cadena), cadena_sin_caracteres_especiales);
+}
+
+void generar_codigo_clave_por_nombre(char **curp, char *apellido_paterno, char *apellido_materno, char *nombre){
+    eliminar_caracteres_especiales(&apellido_paterno);
+    eliminar_caracteres_especiales(&apellido_materno);
+    eliminar_caracteres_especiales(&nombre);
+    
+    int longitudMinimaDeCadena = 1;
+    
+    //Tiene dos apellidos
+    if (strlen(apellido_paterno) > longitudMinimaDeCadena && strlen(apellido_materno)> longitudMinimaDeCadena) {
+        //Si el apellido paterno solo contiene un caracter.
+        if (strlen(apellido_paterno) == longitudMinimaDeCadena + 1) {
+            (*curp)[0] = toupper(apellido_paterno[0]);
+            (*curp)[1] = 'X';
+        }else{
+            //Si el apellido paterno contiene mas de un caracter.
+            (*curp)[0] = toupper(apellido_paterno[0]);
+            (*curp)[1] = toupper(apellido_paterno[1]);
+        }
+        (*curp)[2] = toupper(apellido_materno[0]);
+        (*curp)[3] = toupper(nombre[0]);
+        (*curp)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*curp));
+    }else if(strlen(apellido_paterno) == 1){
+        //Tiene solo apellido materno
+        
+        //Si el apellido materno solo contiene un caracter.
+        if (strlen(apellido_materno) == longitudMinimaDeCadena + 1) {
+            (*curp)[0] = toupper(apellido_materno[0]);
+            (*curp)[1] = 'X';
+        }else{
+            //Si el apellido materno contiene mas de un caracter.
+            (*curp)[0] = toupper(apellido_materno[0]);
+            (*curp)[1] = toupper(apellido_materno[1]);
+        }
+        (*curp)[2] = toupper(nombre[0]);
+        (*curp)[3] = toupper(nombre[1]);
+        (*curp)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*curp));
+    }else{
+        //Tienes solo apellido paterno
+        
+        //Si el apellido paterno solo contiene un caracter.
+        if (strlen(apellido_paterno) == longitudMinimaDeCadena + 1) {
+            (*curp)[0] = toupper(apellido_paterno[0]);
+            (*curp)[1] = 'X';
+        }else{
+            //Si el apellido paterno contiene mas de un caracter.
+            (*curp)[0] = toupper(apellido_paterno[0]);
+            (*curp)[1] = toupper(apellido_paterno[1]);
+        }
+        (*curp)[2] = toupper(nombre[0]);
+        (*curp)[3] = toupper(nombre[1]);
+        (*curp)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*curp));
+    }
+}
+
+
+
 
 #endif /* funciones_h */
--- Generador-RFC-CURP/main.c
+++ Generador-RFC-CURP/main.c
@@ -36,74 +36,25 @@ char* removerTilde( char* str ) {
 }
 
 int main(int argc, const char * argv[]) {
-    char nombre[50], apellidoP[50], apellidoM[50], clavePorNombre[5];
+    char *nombre = (char*)malloc(50*sizeof(char));
+    char *apellido_paterno = (char*)malloc(50*sizeof(char));
+    char *apellido_materno = (char*)malloc(50*sizeof(char));
+    char *curp = (char*)malloc(10*sizeof(char));
     //char alfabetoLatinoEstandar[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
     //Esto es porque aunque se omita alguna entrada el 'enter' se registra como un caracter.
-    int longitudMinimaDeCadena = 1;
     
     printf("Apellido Paterno : ");
-    fgets(apellidoP,50,stdin);
-    memcpy(apellidoP, eliminarCaracteresEspeciales(apellidoP), strlen(apellidoP)+1);
-    //memcpy(apellidoP, removerTilde(apellidoP), strlen(apellidoP+1));
+    fgets(apellido_paterno,50,stdin);
     
     printf("Apellido Materno : ");
-    fgets(apellidoM,50,stdin);
-    memcpy(apellidoM, eliminarCaracteresEspeciales(apellidoM), strlen(apellidoM)+1);
-    //memcpy(apellidoM, removerTilde(apellidoM), strlen(apellidoM+1));
+    fgets(apellido_materno,50,stdin);
     
     printf("Nombre : ");
     fgets(nombre,50,stdin);
-    memcpy(nombre, eliminarCaracteresEspeciales(nombre), strlen(nombre)+1);
-    //memcpy(nombre, removerTilde(nombre), strlen(nombre+1));
     
-    //Tiene dos apellidos
-    if (strlen(apellidoP) > longitudMinimaDeCadena && strlen(apellidoM)> longitudMinimaDeCadena) {
-        //Si el apellido paterno solo contiene un caracter.
-        if (strlen(apellidoP) == longitudMinimaDeCadena + 1) {
-            clavePorNombre[0] = toupper(apellidoP[0]);
-            clavePorNombre[1] = 'X';
-        }else{
-            //Si el apellido paterno contiene mas de un caracter.
-            clavePorNombre[0] = toupper(apellidoP[0]);
-            clavePorNombre[1] = toupper(apellidoP[1]);
-        }
-        clavePorNombre[2] = toupper(apellidoM[0]);
-        clavePorNombre[3] = toupper(nombre[0]);
-        clavePorNombre[4] = '\0';
-        printf("Clave por nombre: %s\n", clavePorNombre);
-    }else if(strlen(apellidoP) == 1){
-        //Tiene solo apellido materno
-        
-        //Si el apellido materno solo contiene un caracter.
-        if (strlen(apellidoM) == longitudMinimaDeCadena + 1) {
-            clavePorNombre[0] = toupper(apellidoM[0]);
-            clavePorNombre[1] = 'X';
-        }else{
-            //Si el apellido materno contiene mas de un caracter.
-            clavePorNombre[0] = toupper(apellidoM[0]);
-            clavePorNombre[1] = toupper(apellidoM[1]);
-        }
-        clavePorNombre[2] = toupper(nombre[0]);
-        clavePorNombre[3] = toupper(nombre[1]);
-        clavePorNombre[4] = '\0';
-        printf("Clave por nombre: %s\n", clavePorNombre);
-    }else{
-        //Tienes solo apellido paterno
-        
-        //Si el apellido paterno solo contiene un caracter.
-        if (strlen(apellidoP) == longitudMinimaDeCadena + 1) {
-            clavePorNombre[0] = toupper(apellidoP[0]);
-            clavePorNombre[1] = 'X';
-        }else{
-            //Si el apellido paterno contiene mas de un caracter.
-            clavePorNombre[0] = toupper(apellidoP[0]);
-            clavePorNombre[1] = toupper(apellidoP[1]);
-        }
-        clavePorNombre[2] = toupper(nombre[0]);
-        clavePorNombre[3] = toupper(nombre[1]);
-        clavePorNombre[4] = '\0';
-        printf("Clave por nombre: %s\n", clavePorNombre);
-    }
+    generar_codigo_clave_por_nombre(&curp, apellido_paterno, apellido_materno, nombre);
+    
+    
     
     return 0;
 }
