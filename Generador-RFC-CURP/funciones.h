b5c5814bd6737f4f2beb13efdf5747a89c30db4c
GaboGomez09 <gomezgabriel09@gmail.com>
2017-03-09 09:48:21 -0600

    Se implemento la función que crea la clave por fecha

--- Generador-RFC-CURP/funciones.h
+++ Generador-RFC-CURP/funciones.h
@@ -22,7 +22,7 @@ void eliminar_caracteres_especiales(char **cadena){
     strcpy((*cadena), cadena_sin_caracteres_especiales);
 }
 
-void generar_codigo_clave_por_nombre(char **curp, char *apellido_paterno, char *apellido_materno, char *nombre){
+void generar_codigo_clave_por_nombre(char **clave_por_nombre, char *apellido_paterno, char *apellido_materno, char *nombre){
     eliminar_caracteres_especiales(&apellido_paterno);
     eliminar_caracteres_especiales(&apellido_materno);
     eliminar_caracteres_especiales(&nombre);
@@ -33,53 +33,125 @@ void generar_codigo_clave_por_nombre(char **curp, char *apellido_paterno, char *
     if (strlen(apellido_paterno) > longitudMinimaDeCadena && strlen(apellido_materno)> longitudMinimaDeCadena) {
         //Si el apellido paterno solo contiene un caracter.
         if (strlen(apellido_paterno) == longitudMinimaDeCadena + 1) {
-            (*curp)[0] = toupper(apellido_paterno[0]);
-            (*curp)[1] = 'X';
+            (*clave_por_nombre)[0] = toupper(apellido_paterno[0]);
+            (*clave_por_nombre)[1] = 'X';
         }else{
             //Si el apellido paterno contiene mas de un caracter.
-            (*curp)[0] = toupper(apellido_paterno[0]);
-            (*curp)[1] = toupper(apellido_paterno[1]);
+            (*clave_por_nombre)[0] = toupper(apellido_paterno[0]);
+            (*clave_por_nombre)[1] = toupper(apellido_paterno[1]);
         }
-        (*curp)[2] = toupper(apellido_materno[0]);
-        (*curp)[3] = toupper(nombre[0]);
-        (*curp)[4] = '\0';
-        printf("Clave por nombre: %s\n", (*curp));
+        (*clave_por_nombre)[2] = toupper(apellido_materno[0]);
+        (*clave_por_nombre)[3] = toupper(nombre[0]);
+        (*clave_por_nombre)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*clave_por_nombre));
     }else if(strlen(apellido_paterno) == 1){
         //Tiene solo apellido materno
         
         //Si el apellido materno solo contiene un caracter.
         if (strlen(apellido_materno) == longitudMinimaDeCadena + 1) {
-            (*curp)[0] = toupper(apellido_materno[0]);
-            (*curp)[1] = 'X';
+            (*clave_por_nombre)[0] = toupper(apellido_materno[0]);
+            (*clave_por_nombre)[1] = 'X';
         }else{
             //Si el apellido materno contiene mas de un caracter.
-            (*curp)[0] = toupper(apellido_materno[0]);
-            (*curp)[1] = toupper(apellido_materno[1]);
+            (*clave_por_nombre)[0] = toupper(apellido_materno[0]);
+            (*clave_por_nombre)[1] = toupper(apellido_materno[1]);
         }
-        (*curp)[2] = toupper(nombre[0]);
-        (*curp)[3] = toupper(nombre[1]);
-        (*curp)[4] = '\0';
-        printf("Clave por nombre: %s\n", (*curp));
+        (*clave_por_nombre)[2] = toupper(nombre[0]);
+        (*clave_por_nombre)[3] = toupper(nombre[1]);
+        (*clave_por_nombre)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*clave_por_nombre));
     }else{
         //Tienes solo apellido paterno
         
         //Si el apellido paterno solo contiene un caracter.
         if (strlen(apellido_paterno) == longitudMinimaDeCadena + 1) {
-            (*curp)[0] = toupper(apellido_paterno[0]);
-            (*curp)[1] = 'X';
+            (*clave_por_nombre)[0] = toupper(apellido_paterno[0]);
+            (*clave_por_nombre)[1] = 'X';
         }else{
             //Si el apellido paterno contiene mas de un caracter.
-            (*curp)[0] = toupper(apellido_paterno[0]);
-            (*curp)[1] = toupper(apellido_paterno[1]);
+            (*clave_por_nombre)[0] = toupper(apellido_paterno[0]);
+            (*clave_por_nombre)[1] = toupper(apellido_paterno[1]);
         }
-        (*curp)[2] = toupper(nombre[0]);
-        (*curp)[3] = toupper(nombre[1]);
-        (*curp)[4] = '\0';
-        printf("Clave por nombre: %s\n", (*curp));
+        (*clave_por_nombre)[2] = toupper(nombre[0]);
+        (*clave_por_nombre)[3] = toupper(nombre[1]);
+        (*clave_por_nombre)[4] = '\0';
+        printf("Clave por nombre: %s\n", (*clave_por_nombre));
     }
 }
 
+void string_to_upper(char **cadena){
+    for (int i = 0; i < strlen(*cadena); i++) {
+        (*cadena)[i] = toupper((*cadena)[i]);
+    }
+}
 
+int mes_a_numero(char *mes){
+    int numero_de_mes = 0;
+    string_to_upper(&mes);
+    
+    if (!strcmp(mes, "ENERO")) {
+        numero_de_mes = 1;
+    }else if (!strcmp(mes, "FEBRERO")) {
+        numero_de_mes = 2;
+    }else if (!strcmp(mes, "MARZO")) {
+        numero_de_mes = 3;
+    }else if (!strcmp(mes, "ABRIL")) {
+        numero_de_mes = 4;
+    }else if (!strcmp(mes, "MAYO")) {
+        numero_de_mes = 5;
+    }else if (!strcmp(mes, "JUNIO")) {
+        numero_de_mes = 6;
+    }else if (!strcmp(mes, "JULIO")) {
+        numero_de_mes = 7;
+    }else if (!strcmp(mes, "AGOSTO")) {
+        numero_de_mes = 8;
+    }else if (!strcmp(mes, "SEPTIEMBRE")) {
+        numero_de_mes = 9;
+    }else if (!strcmp(mes, "OCTUBRE")) {
+        numero_de_mes = 10;
+    }else if (!strcmp(mes, "NOVIEMBRE")) {
+        numero_de_mes = 11;
+    }else if (!strcmp(mes, "DICIEMBRE")) {
+        numero_de_mes = 12;
+    }else{
+        printf("Mes no reconocido.\n");
+    }
+    
+    return numero_de_mes;
+}
+
+void numero_a_cadena(int numero, char **cadena){
+    if (numero < 10) {
+        char num_temp[2];
+        sprintf(num_temp, "%d", numero);
+        sprintf(*cadena, "%d", 0);
+        strcat(*cadena, num_temp);
+    }else{
+        sprintf(*cadena, "%d",numero);
+    }
+}
+
+void generar_clave_por_fecha_nacimiento(char **clave_por_fecha, int anio, char *mes, int dia){
+    int num_mes = 0, num_anio = 0;
+    num_mes = mes_a_numero(mes);
+    num_anio = anio % 100;
+    
+    char *numero_mes = (char*)malloc(3*sizeof(char));
+    char *numero_anio = (char*)malloc(3*sizeof(char));
+    char *numero_dia = (char*)malloc(3*sizeof(char));
+    
+    numero_a_cadena(num_anio, &numero_anio);
+    numero_a_cadena(num_mes, &numero_mes);
+    numero_a_cadena(dia, &numero_dia);
+    
+    strcat(*clave_por_fecha, numero_anio);
+    strcat(*clave_por_fecha, numero_mes);
+    strcat(*clave_por_fecha, numero_dia);
+}
+
+void generar_clave_homonimia(){
+    
+}
 
 
 #endif /* funciones_h */
--- Generador-RFC-CURP/main.c
+++ Generador-RFC-CURP/main.c
@@ -12,19 +12,6 @@
 #include <stdlib.h>
 #include "funciones.h"
 
-char* eliminarCaracteresEspeciales(char *cadena){
-    char *cadenaSinCaracteresEspeciales = (char*)malloc(50*sizeof(char));
-    int j = 0;
-    for (int i = 0; i < strlen(cadena); i++) {
-        if (ispunct(cadena[i])) {
-        }else{
-            cadenaSinCaracteresEspeciales[j] = cadena[i];
-            j++;
-        }
-    }
-    return cadenaSinCaracteresEspeciales;
-}
-
 char* removerTilde( char* str ) {
     //char *p = (char*)malloc(50*sizeof(char));
     for (int i = 0; i < strlen(str); i++) {
@@ -36,24 +23,40 @@ char* removerTilde( char* str ) {
 }
 
 int main(int argc, const char * argv[]) {
-    char *nombre = (char*)malloc(50*sizeof(char));
-    char *apellido_paterno = (char*)malloc(50*sizeof(char));
-    char *apellido_materno = (char*)malloc(50*sizeof(char));
-    char *curp = (char*)malloc(10*sizeof(char));
+//    char *nombre = (char*)malloc(50*sizeof(char));
+//    char *apellido_paterno = (char*)malloc(50*sizeof(char));
+//    char *apellido_materno = (char*)malloc(50*sizeof(char));
+//    char *clave_por_nombre = (char*)malloc(10*sizeof(char));
+//    char *clave_por_fecha = (char*)malloc(10*sizeof(char));
+//    int anio = 0;
+//    char *mes = (char*)malloc(15*sizeof(char));
+//    int dia = 0;
     //char alfabetoLatinoEstandar[27] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
     //Esto es porque aunque se omita alguna entrada el 'enter' se registra como un caracter.
     
-    printf("Apellido Paterno : ");
-    fgets(apellido_paterno,50,stdin);
+//    printf("Apellido Paterno : ");
+//    fgets(apellido_paterno,50,stdin);
+//    
+//    printf("Apellido Materno : ");
+//    fgets(apellido_materno,50,stdin);
+//    
+//    printf("Nombre : ");
+//    fgets(nombre,50,stdin);
     
-    printf("Apellido Materno : ");
-    fgets(apellido_materno,50,stdin);
+//    printf("Anio en que nacio: ");
+//    scanf("%d", &anio);
+//    
+//    printf("Mes en que nacio: ");
+//    scanf("%s", mes);
+//    
+//    printf("Dia en que nacio: ");
+//    scanf("%d", &dia);
     
-    printf("Nombre : ");
-    fgets(nombre,50,stdin);
+//    generar_codigo_clave_por_nombre(&clave_por_nombre, apellido_paterno, apellido_materno, nombre);
     
-    generar_codigo_clave_por_nombre(&curp, apellido_paterno, apellido_materno, nombre);
-    
+//    generar_clave_por_fecha_nacimiento(&clave_por_fecha, anio, mes, dia);
+//    
+//    printf("%s", clave_por_fecha);
     
     
     return 0;
